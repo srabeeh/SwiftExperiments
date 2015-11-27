9913df6d09d5eeaa093c8472206b2f2f04733288
Sam Rabeeh <srabeeh@larci.com>
2015-11-27 07:17:14 -0700

    First IOS swift app from scratch, simple stuff.

--- tapper/ViewController.swift
+++ tapper/ViewController.swift
@@ -9,10 +9,74 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // Properties
+    var maxTaps = 0
+    var currentTaps = 0
+    
 
+    // Outlets
+    @IBOutlet weak var logoImg: UIImageView!
+    @IBOutlet weak var numberOfTaps: UITextField!
+    @IBOutlet weak var playBtn: UIButton!
+    
+    @IBOutlet weak var tapBtn: UIButton!
+    @IBOutlet weak var tapsLabel: UILabel!
+    
+    @IBAction func onPlayBtnPressed(sender: UIButton!){
+
+        
+        if numberOfTaps.text != nil && numberOfTaps.text != ""{
+            logoImg.hidden = true
+            playBtn.hidden = true
+            numberOfTaps.hidden = true
+            
+            tapBtn.hidden = false
+            tapsLabel.hidden = false
+            maxTaps = Int(numberOfTaps.text!)!
+            currentTaps = 0
+            updateTapsLabel()
+        }
+    }
+    
+    @IBAction func onCoinTapped(sender: UIButton!){
+        currentTaps++
+        updateTapsLabel()
+        if isGameOver(){
+            restartGame()
+        }
+        
+    }
+    
+    func restartGame(){
+        maxTaps = 0
+        tapsLabel.text = ""
+        logoImg.hidden = false
+        playBtn.hidden = false
+        numberOfTaps.hidden = false
+        
+        tapBtn.hidden = true
+        tapsLabel.hidden = true
+    }
+    
+    func updateTapsLabel(){
+        tapsLabel.text = "\(currentTaps) Taps"
+    }
+    
+    func isGameOver() -> Bool{
+        if currentTaps >= maxTaps{
+            return true
+        }
+        else{
+            return false
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tapBtn.hidden = true
+        tapsLabel.hidden = true
     }
 
     override func didReceiveMemoryWarning() {
